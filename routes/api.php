4e70f67973d3247a164c2c60e455a5f7ca763bea
rcsio <78203730+rcsio@users.noreply.github.com>
2024-04-07 13:51:48 +0400

    Create routes for creating and deleting tokens

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/tokens', function (LoginRequest $request) {
+    $request->authenticate();
+    $token = $request->user()->createToken('authToken');
+    return ['token' => $token->plainTextToken];
+});
+
+Route::delete('/tokens', function (Request $request) {
+    $request->user()->currentAccessToken()->delete();
+})->middleware(['auth:sanctum']);
